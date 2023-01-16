8802921f270d66fb038f2b0aa788dac3779a510d
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-01-16 21:43:48 +0530

    Create Solution.java

---
+++ Leet Code/Problems/242. Valid Anagram/Solution.java
@@ -0,0 +1,8 @@
+class Solution
+{
+    public boolean isAnagram(String s, String t)
+    {
+        return Arrays.equals
+        (s.chars().sorted().toArray(),t.chars().sorted().toArray());
+    }
+}
