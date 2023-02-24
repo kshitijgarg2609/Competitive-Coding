5cfb428653528ee3ef77400abadc98f16a91cc70
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-02-24 22:40:18 +0530

    Create Solution.java

---
+++ Leet Code/Problems/844. Backspace String Compare/Solution.java
@@ -0,0 +1,33 @@
+class Solution
+{
+    public boolean backspaceCompare(String s, String t)
+    {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for(char c : s.toCharArray())
+        {
+            if(c=='#')
+            {
+                if(sb1.length()!=0)
+                {
+                    sb1.deleteCharAt(sb1.length()-1);
+                }
+                continue;
+            }
+            sb1.append(c);
+        }
+        for(char c : t.toCharArray())
+        {
+            if(c=='#')
+            {
+                if(sb2.length()!=0)
+                {
+                    sb2.deleteCharAt(sb2.length()-1);
+                }
+                continue;
+            }
+            sb2.append(c);
+        }
+        return sb1.toString().equals(sb2.toString());
+    }
+}
