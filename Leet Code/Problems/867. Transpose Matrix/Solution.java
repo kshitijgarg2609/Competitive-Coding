acb51feb81ee6a4999d96c590425b1e57f3f22c5
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-02-25 10:30:47 +0530

    Update Solution.java

--- Leet Code/Problems/867. Transpose Matrix/Solution.java
+++ Leet Code/Problems/867. Transpose Matrix/Solution.java
@@ -5,18 +5,11 @@ class Solution
         int r=matrix.length;
         int c=matrix[0].length;
         int arr[][] = new int[c][r];
-        int ai=0;
-        int aj=0;
         for(int i=0;i<r;i++)
         {
             for(int j=0;j<c;j++)
             {
-                arr[ai++][aj]=matrix[i][j];
-                if(ai==c)
-                {
-                    ai=0;
-                    aj++;
-                }
+                arr[j][i]=matrix[i][j];
             }
         }
         return arr;
