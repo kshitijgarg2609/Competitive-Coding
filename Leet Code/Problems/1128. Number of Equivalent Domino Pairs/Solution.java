efdb5fb02661e6adabf32ef450c0cd4bf035d8be
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-02-26 00:08:07 +0530

    Update Solution.java

--- Leet Code/Problems/1128. Number of Equivalent Domino Pairs/Solution.java
+++ Leet Code/Problems/1128. Number of Equivalent Domino Pairs/Solution.java
@@ -1,1 +1,21 @@
-
+class Solution
+{
+    public int numEquivDominoPairs(int[][] dominoes)
+    {
+        int cnt=0;
+        Map<List<Integer>,Integer> map = new HashMap<>();
+        for(int d[] : dominoes)
+        {
+            List<Integer> dom = Arrays.asList(Integer.min(d[0],d[1]),Integer.max(d[0],d[1]));
+            map.put(dom,map.getOrDefault(dom,0)+1);
+        }
+        for(int x : map.values())
+        {
+            if(x>=2)
+            {
+                cnt+=(x*(x-1))/2;
+            }
+        }
+        return cnt;
+    }
+}
