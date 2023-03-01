5ca1ff13771b3c5125e87b6043033755cd9998d7
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-03-01 14:42:53 +0530

    Create Solution.java

---
+++ Leet Code/Problems/225. Implement Stack using Queues/Solution.java
@@ -0,0 +1,37 @@
+class MyStack
+{
+    Deque<Integer> q;
+    public MyStack()
+    {
+        q = new ArrayDeque<>();
+    }
+    
+    public void push(int x)
+    {
+        q.addLast(x);
+    }
+    
+    public int pop()
+    {
+        return q.removeLast();
+    }
+    
+    public int top()
+    {
+        return q.peekLast();
+    }
+    
+    public boolean empty()
+    {
+        return q.peekLast()==null;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
