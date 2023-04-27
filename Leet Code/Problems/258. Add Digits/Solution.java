03860406d19570f55ccc9af2852d56b1bcea0974
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-04-27 16:21:51 +0530

    Update Solution.java

--- Leet Code/Problems/258. Add Digits/Solution.java
+++ Leet Code/Problems/258. Add Digits/Solution.java
@@ -2,11 +2,6 @@ class Solution
 {
     public int addDigits(int num)
     {
-        String number=String.valueOf(num);
-        while(number.length()!=1)
-        {
-            number=String.valueOf((number.chars().sum())-(number.length()*'0'));
-        }
-        return Integer.valueOf(number).intValue();
+        return (num==0)?0:((num%9==0)?9:(num%9));
     }
 }
