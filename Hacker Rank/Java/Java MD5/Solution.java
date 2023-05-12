58184f7fa5f820316e0663a9657903ef2a82fb52
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-05-13 00:12:08 +0530

    Create Solution.java

---
+++ Hacker Rank/Java/Java MD5/Solution.java
@@ -0,0 +1,20 @@
+import java.security.*;
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte msg[] = md.digest(input.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for(byte b : msg)
+        {
+            sb.append(String.format("%02x",Integer.valueOf(Byte.toUnsignedInt(b))));
+        }
+        System.out.println(sb);
+    }
+}
