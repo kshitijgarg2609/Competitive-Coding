dc5eac644b8b07f81439aca6304a52e7989cc134
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-05-20 23:24:37 +0530

    Update Solution.java

--- Leet Code/Problems/37. Sudoku Solver/Solution.java
+++ Leet Code/Problems/37. Sudoku Solver/Solution.java
@@ -1,88 +1,69 @@
-import java.awt.*;
 class Solution
 {
     public void solveSudoku(char[][] board)
     {
-      LinkedList<Point> emp = new LinkedList<Point>();
-      for(int i=0;i<9;i++)
-      {
-        for(int j=0;j<9;j++)
+        List<List<Integer>> fill = new ArrayList<>();
+        boolean row[][] = new boolean[9][9];
+        boolean col[][] = new boolean[9][9];
+        boolean block[][] = new boolean[9][9];
+        for(int i=0;i<9;i++)
         {
-          if(board[i][j]=='.')
-          {
-            emp.add(new Point(i,j));
-          }
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    fill.add(Arrays.asList(i,j));
+                    continue;
+                }
+                row[i][board[i][j]-'0'-1]=true;
+                col[j][board[i][j]-'0'-1]=true;
+                block[(3*(i/3))+(j/3)][board[i][j]-'0'-1]=true;
+            }
         }
-      }
-      int i=0;
-      while(i!=emp.size())
-      {
-        Point pop=emp.get(i);
-        if(board[pop.x][pop.y]=='9')
+        int i=0;
+        while(i<fill.size())
         {
-          board[pop.x][pop.y]='.';
-          i--;
-          continue;
-        }
-        boolean val=false;
-        trial:
-        for(int k=(board[pop.x][pop.y]=='.')?'1':(char)(board[pop.x][pop.y]+1);k<='9';k++)
-        {
-          val=true;
-          board[pop.x][pop.y]=(char)(k);
-          for(int j=0;j<9;j++)
-          {
-            if(pop.y==j)
+            List<Integer> crd = fill.get(i);
+            int block_indx = (3*(crd.get(0)/3))+(crd.get(1)/3);
+            char ch=board[crd.get(0)][crd.get(1)];
+            if(ch!='.')
             {
-              continue;
+                row[crd.get(0)][ch-'0'-1]=false;
+                col[crd.get(1)][ch-'0'-1]=false;
+                block[block_indx][ch-'0'-1]=false;
             }
-            if(board[pop.x][pop.y]==board[pop.x][j])
+            if(ch=='9')
             {
-              val=false;
-              continue trial;
-            }
-          }
-          for(int j=0;j<9;j++)
-          {
-            if(pop.x==j)
-            {
-              continue;
-            }
-            if(board[pop.x][pop.y]==board[j][pop.y])
-            {
-              val=false;
-              continue trial;
-            }
-          }
-          int a=(pop.x/3)*3;
-          int b=(pop.y/3)*3;
-          for(int p=a;p<a+3;p++)
-          {
-            for(int q=b;q<b+3;q++)
-            {
-              if(p==pop.x && q==pop.y)
-              {
+                board[crd.get(0)][crd.get(1)]='.';
+                i--;
                 continue;
-              }
-              if(board[pop.x][pop.y]==board[p][q])
-              {
-                val=false;
-                continue trial;
-              }
             }
-          }
-          if(val)
-          {
-            break;
-          }
+            else if(ch=='.')
+            {
+                ch='1';
+            }
+            else
+            {
+                ch=(char)(ch+1);
+            }
+            boolean flg=true;
+            for(char c=ch;flg && c<='9';c=(char)(c+1))
+            {
+                if(!row[crd.get(0)][c-'0'-1] && !col[crd.get(1)][c-'0'-1] && !block[block_indx][c-'0'-1])
+                {
+                    board[crd.get(0)][crd.get(1)]=c;
+                    row[crd.get(0)][c-'0'-1]=true;
+                    col[crd.get(1)][c-'0'-1]=true;
+                    block[block_indx][c-'0'-1]=true;
+                    i++;
+                    flg=false;
+                }
+            }
+            if(flg)
+            {
+                board[crd.get(0)][crd.get(1)]='.';
+                i--;
+            }
         }
-        if(!val)
-        {
-          board[pop.x][pop.y]='.';
-          i--;
-          continue;
-        }
-        i++;
-      }
     }
 }
