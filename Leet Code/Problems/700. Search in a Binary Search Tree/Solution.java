b3a9a9a341eb44b328627f022b930494fb406685
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-05-29 15:20:57 +0530

    Create Solution.java

---
+++ Leet Code/Problems/700. Search in a Binary Search Tree/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution
+{
+    public TreeNode searchBST(TreeNode root, int val)
+    {
+        TreeNode ptr=root;
+        while(ptr!=null)
+        {
+            if(val>ptr.val)
+            {
+                ptr=ptr.right;
+            }
+            else if(val<ptr.val)
+            {
+                ptr=ptr.left;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return ptr;
+    }
+}
