77af1be72b402e45c4c56cb5829ea87083f2d622
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-06-03 01:07:29 +0530

    Create Solution.cpp

---
+++ Hacker Rank/C++/Preprocessor Solution/Solution.cpp
@@ -0,0 +1,33 @@
+#define toStr(result) #result
+#define io(v) cin>>v
+#define FUNCTION(function,operation) void function(int &a, int b) {if(b operation a){a=b;}}
+#define INF (int)(1E8+1)
+#define foreach(v,i) for(int i=0;i<v.size();i++)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result =) <<' '<< ans;
+	return 0;
+
+}
