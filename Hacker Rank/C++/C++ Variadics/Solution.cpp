6b32fe4c413ce42793d471c8fa87194ee12d0c02
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-06-05 23:31:30 +0530

    Create Solution.cpp

---
+++ Hacker Rank/C++/C++ Variadics/Solution.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+template <bool...bits>
+int reversed_binary_value() {
+    int rev_sum=0;
+    int pow=1;
+    for(bool b : {bits...}) {
+        if(b) {
+            rev_sum+=pow;
+        }
+        pow*=2;
+    }
+    return rev_sum;
+}
+
+
+template <int n, bool...digits>
+struct CheckValues {
+  	static void check(int x, int y)
+  	{
+    	CheckValues<n-1, 0, digits...>::check(x, y);
+    	CheckValues<n-1, 1, digits...>::check(x, y);
+  	}
+};
+
+template <bool...digits>
+struct CheckValues<0, digits...> {
+  	static void check(int x, int y)
+  	{
+    	int z = reversed_binary_value<digits...>();
+    	std::cout << (z+64*y==x);
+  	}
+};
+
+int main()
+{
+  	int t; std::cin >> t;
+
+  	for (int i=0; i!=t; ++i) {
+		int x, y;
+    	cin >> x >> y;
+    	CheckValues<6>::check(x, y);
+    	cout << "\n";
+  	}
+}
