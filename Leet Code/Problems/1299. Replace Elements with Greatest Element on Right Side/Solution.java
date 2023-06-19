430daadcac20b19146e0f6f2e556a78d70ff9e1c
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-06-20 01:29:37 +0530

    Create Solution.java

---
+++ Leet Code/Problems/1299. Replace Elements with Greatest Element on Right Side/Solution.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int[] replaceElements(int[] arr)
+    {
+        int max=-1;
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            if(arr[i]>max)
+            {
+                int tmp=arr[i];
+                arr[i]=max;
+                max=tmp;
+            }
+            else
+            {
+                arr[i]=max;
+            }
+        }
+        return arr;
+    }
+}
