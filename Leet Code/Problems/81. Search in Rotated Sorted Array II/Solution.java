8bd0cddd94162e67ca9129cbdbb71979dc8d4da7
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-08-10 15:57:01 +0530

    Create Solution.java

---
+++ Leet Code/Problems/81. Search in Rotated Sorted Array II/Solution.java
@@ -0,0 +1,43 @@
+class Solution
+{
+    public boolean search(int[] nums, int target)
+    {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            if(nums[mid]==nums[left] && nums[mid]!=target)
+            {
+                left++;
+            }
+            else if(nums[mid]>=nums[left] && nums[mid]!=target)
+            {
+                if(target<nums[mid] && target>=nums[left])
+                {
+                    right=mid-1;
+                }
+                else
+                {
+                    left=mid+1;
+                }
+            }
+            else if(nums[mid]<=nums[right] && nums[mid]!=target)
+            {
+                if(target>nums[mid] && target<=nums[right]) 
+                {
+                    left=mid+1;
+                }
+                else
+                {
+                    right=mid-1;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
