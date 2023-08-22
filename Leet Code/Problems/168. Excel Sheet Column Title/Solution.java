d65259c1086bc7c33d48d7681ee57cbd96b25bbe
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-08-22 10:31:48 +0530

    Update Solution.java

--- Leet Code/Problems/168. Excel Sheet Column Title/Solution.java
+++ Leet Code/Problems/168. Excel Sheet Column Title/Solution.java
@@ -6,10 +6,9 @@ class Solution
         int n=columnNumber;
         while(n>0)
         {
-            char c=(n%26==0)?'Z':(char)((n%26)-1+'A');
-            sb.append(c);
-            System.out.println(n%26);
-            n=(n-(c-'A'))/26;
+            n--;
+            sb.append((char)((n%26)+'A'));
+            n/=26;
         }
         return sb.reverse().toString();
     }
