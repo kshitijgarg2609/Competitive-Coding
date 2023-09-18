ff9f4fb20d43d90b5534e5612975699c8db060b6
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-09-18 09:14:35 +0530

    Update Solution.java

--- Leet Code/Problems/1337. The K Weakest Rows in a Matrix/Solution.java
+++ Leet Code/Problems/1337. The K Weakest Rows in a Matrix/Solution.java
@@ -2,35 +2,15 @@ class Solution
 {
     public int[] kWeakestRows(int[][] mat, int k)
     {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>()
-        {
-            public int compare(int a[],int b[])
-            {
-                int rank_match=Integer.compare(a[0],b[0]);
-                return (rank_match!=0)?rank_match:(Integer.compare(a[1],b[1]));
-            }
-        });
-        int r=mat.length;
-        int c=mat[0].length;
-        for(int i=0;i<r;i++)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt((int a[])->a[0]).thenComparingInt((int a[])->a[1]));
+        for(int i=0;i<mat.length;i++)
         {
             int tuple[] = new int[]{0,i};
-            for(int j=0;j<c;j++)
-            {
-                if(mat[i][j]==0)
-                {
-                    break;
-                }
-                tuple[0]++;
-            }
+            for(int j=0;j<mat[0].length && mat[i][j]==1;tuple[0]++,j++);
             pq.add(tuple);
         }
         int ans[] = new int[k];
-        for(int i=0;i<k;i++)
-        {
-            int tuple[] = pq.poll();
-            ans[i]=tuple[1];
-        }
+        for(int i=0;i<k;ans[i++]=pq.poll()[1]);
         return ans;
     }
 }
