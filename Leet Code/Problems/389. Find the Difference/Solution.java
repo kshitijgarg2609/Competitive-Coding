c3f779b033c6a902befa463983599d323a442020
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-09-25 10:22:06 +0530

    Update Solution.java

--- Leet Code/Problems/389. Find the Difference/Solution.java
+++ Leet Code/Problems/389. Find the Difference/Solution.java
@@ -2,23 +2,6 @@ class Solution
 {
     public char findTheDifference(String s, String t)
     {
-        Map<Integer,Long> map = t.chars().boxed().collect
-        (Collectors.groupingBy(Function.identity(),Collectors.counting()));
-        for(char c : s.toCharArray())
-        {
-            if(map.containsKey(Integer.valueOf(c)))
-            {
-                map.computeIfPresent(Integer.valueOf(c),(key,value)->value-1);
-                if(map.get(Integer.valueOf(c))==0)
-                {
-                    map.remove(Integer.valueOf(c));
-                }
-            }
-            else
-            {
-                return c;
-            }
-        }
-        return (char)(map.entrySet().iterator().next().getKey().intValue());
+        return (char)(t.chars().sum()-s.chars().sum());
     }
 }
