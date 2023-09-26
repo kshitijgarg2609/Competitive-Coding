3417da69c74c5c5e95ed9df69ccc3d5a1c88a504
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-09-26 10:37:39 +0530

    Create Solution.java

---
+++ Leet Code/Problems/316. Remove Duplicate Letters/Solution.java
@@ -0,0 +1,28 @@
+class Solution
+{
+    public String removeDuplicateLetters(String s)
+    {
+        int freq[] = new int[26];
+        for(char c : s.toCharArray())
+        {
+            freq[c-'a']++;
+        }
+        boolean vis[] = new boolean[26];
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray())
+        {
+            if(vis[c-'a'])
+            {
+                freq[c-'a']--;
+                continue;
+            }
+            for(;!st.isEmpty() && c<st.peek() && freq[st.peek()-'a']>0;vis[st.pop()-'a']=false);
+            st.push(c);
+            freq[c-'a']--;
+            vis[c-'a']=true;
+        }
+        StringBuilder sb = new StringBuilder();
+        st.stream().forEach(x->sb.append(x));
+        return sb.toString();
+    }
+}
