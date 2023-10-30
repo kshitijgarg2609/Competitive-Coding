8f5dfde26ca4e628c4f1224bcd88db1fc1152fcc
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-10-30 12:58:20 +0530

    Update Solution.java

--- Leet Code/Problems/4. Median of Two Sorted Arrays/Solution.java
+++ Leet Code/Problems/4. Median of Two Sorted Arrays/Solution.java
@@ -5,14 +5,7 @@ class Solution
         Deque<Integer> list = new ArrayDeque<>();
         for(int i=0,j=0,k=0;k<=(nums1.length+nums2.length)/2;k++)
         {
-            if(i<nums1.length && j<nums2.length)
-            {
-                list.add(((nums1[i]<=nums2[j])?nums1[i++]:nums2[j++]));
-            }
-            else
-            {
-                list.add(((i<nums1.length)?nums1[i++]:nums2[j++]));
-            }
+            list.add((j==nums2.length || (i<nums1.length && nums1[i]<=nums2[j]))?nums1[i++]:nums2[j++]);
             if(list.size()>2)
             {
                 list.removeFirst();
