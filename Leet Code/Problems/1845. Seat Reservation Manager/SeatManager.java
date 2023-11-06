01a8defd31fb74514a728b74aadd294ad4340d14
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-11-06 09:28:13 +0530

    Create SeatManager.java

---
+++ Leet Code/Problems/1845. Seat Reservation Manager/SeatManager.java
@@ -0,0 +1,26 @@
+class SeatManager
+{
+    private PriorityQueue<Integer> pq;
+    public SeatManager(int n)
+    {
+        pq = new PriorityQueue<>(n);
+        IntStream.rangeClosed(1,n).forEach(x->pq.add(x));
+    }
+    
+    public int reserve()
+    {
+        return pq.poll();
+    }
+    
+    public void unreserve(int seatNumber)
+    {
+        pq.add(seatNumber);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
