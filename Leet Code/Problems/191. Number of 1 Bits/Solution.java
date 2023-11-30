996c6410f4d457d827205ade56f4c57ced4b0bec
Kshitij Garg <kshitijgarg2609@gmail.com>
2023-11-30 13:47:16 +0530

    Update Solution.java

--- Leet Code/Problems/191. Number of 1 Bits/Solution.java
+++ Leet Code/Problems/191. Number of 1 Bits/Solution.java
@@ -3,6 +3,6 @@ public class Solution
     // you need to treat n as an unsigned value
     public int hammingWeight(int n)
     {
-        return (int)Integer.toBinaryString(n).chars().filter(a->a=='1').count();
+        return Integer.bitCount(n);
     }
 }
