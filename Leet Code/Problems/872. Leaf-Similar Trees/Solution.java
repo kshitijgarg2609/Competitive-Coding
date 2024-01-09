93f54c4fae75d46cb380dbd8fa0a541885bceb44
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-01-09 14:51:55 +0530

    Update Solution.java

--- Leet Code/Problems/872. Leaf-Similar Trees/Solution.java
+++ Leet Code/Problems/872. Leaf-Similar Trees/Solution.java
@@ -17,43 +17,30 @@ class Solution
 {
     public boolean leafSimilar(TreeNode root1, TreeNode root2)
     {
-        List<Integer> list1 = new LinkedList<>();
-        List<Integer> list2 = new LinkedList<>();
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
-        TreeNode ptr1=root1;
-        TreeNode ptr2=root2;
-        while(ptr1!=null || !stack1.empty() || ptr2!=null || !stack2.empty())
+        List<List<Integer>> list = new LinkedList<>();
+        for(TreeNode root : Arrays.asList(root1,root2))
         {
-            if(ptr1!=null)
+            List<Integer> leaves = new LinkedList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            for(TreeNode ptr=root;ptr!=null || !stack.isEmpty();)
             {
-                stack1.push(ptr1);
-                ptr1=ptr1.left;
-            }
-            else if(!stack1.empty())
-            {
-                ptr1=stack1.pop();
-                if(ptr1.left==null && ptr1.right==null)
+                if(ptr!=null)
                 {
-                    list1.add(ptr1.val);
+                    stack.push(ptr);
+                    ptr=ptr.left;
                 }
-                ptr1=ptr1.right;
-            }
-            if(ptr2!=null)
-            {
-                stack2.push(ptr2);
-                ptr2=ptr2.left;
-            }
-            else if(!stack2.empty())
-            {
-                ptr2=stack2.pop();
-                if(ptr2.left==null && ptr2.right==null)
+                else if(!stack.isEmpty())
                 {
-                    list2.add(ptr2.val);
+                    ptr=stack.pop();
+                    if(ptr.left==null && ptr.right==null)
+                    {
+                        leaves.add(ptr.val);
+                    }
+                    ptr=ptr.right;
                 }
-                ptr2=ptr2.right;
             }
+            list.add(leaves);
         }
-        return list1.equals(list2);
+        return list.get(0).equals(list.get(1));
     }
 }
