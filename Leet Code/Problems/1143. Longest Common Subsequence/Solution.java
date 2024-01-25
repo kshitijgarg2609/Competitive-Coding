7d2da0ac90396080dacb8a8ced4486feb7f568e6
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-01-25 09:10:57 +0530

    Create Solution.java

---
+++ Leet Code/Problems/1143. Longest Common Subsequence/Solution.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    public int longestCommonSubsequence(String text1, String text2)
+    {
+        int dp[][] = new int[text1.length()][text2.length()];
+        for(int i=0;i<text1.length();i++)
+        {
+            for(int j=0;j<text2.length();j++)
+            {
+                if(text1.charAt(i)==text2.charAt(j))
+                {
+                    dp[i][j]=1+((i>=1 && j>=1)?dp[i-1][j-1]:0);
+                }
+                else
+                {
+                    dp[i][j]=Integer.max((i>=1?dp[i-1][j]:0),(j>=1?dp[i][j-1]:0));
+                }
+            }
+        }
+        return dp[text1.length()-1][text2.length()-1];
+    }
+}
