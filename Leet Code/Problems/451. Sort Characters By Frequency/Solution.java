e6885ab16cfb6062796767cf38250c90136e0007
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-02-07 10:32:08 +0530

    Update Solution.java

--- Leet Code/Problems/451. Sort Characters By Frequency/Solution.java
+++ Leet Code/Problems/451. Sort Characters By Frequency/Solution.java
@@ -2,11 +2,6 @@ class Solution
 {
     public String frequencySort(String s)
     {
-        Map<Character,Integer> map = new HashMap<>();
-        for(char c : s.toCharArray())
-        {
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-        return map.entrySet().stream().sorted((a,b)->Integer.compare(b.getValue(),a.getValue())).flatMap(ent->Stream.generate(()->ent.getKey().toString()).limit(ent.getValue())).collect(Collectors.joining());
+        return s.chars().mapToObj(x->(char)x).collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).entrySet().stream().sorted((a,b)->Long.compare(b.getValue(),a.getValue())).flatMap(ent->Stream.generate(()->ent.getKey().toString()).limit(ent.getValue())).collect(Collectors.joining());
     }
 }
