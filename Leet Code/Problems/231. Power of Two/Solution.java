fe593ed4368200aeaaa40c76e9f237714acdde8c
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-02-20 00:46:00 +0530

    Update Solution.java

--- Leet Code/Problems/231. Power of Two/Solution.java
+++ Leet Code/Problems/231. Power of Two/Solution.java
@@ -2,14 +2,7 @@ class Solution
 {
     public boolean isPowerOfTwo(int n)
     {
-        if(n<=0)
-        {
-            return false;
-        }
-        while((n%2)==0)
-        {
-            n/=2;
-        }
+        for(;n>0 && (n%2)==0;n/=2);
         return n==1;
     }
 }
