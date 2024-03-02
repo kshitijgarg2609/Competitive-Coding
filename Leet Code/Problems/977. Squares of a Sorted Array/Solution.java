3776c87b7d274fbb6caef7fae33096fd23e14bf3
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-03-02 13:41:43 +0530

    Update Solution.java

--- Leet Code/Problems/977. Squares of a Sorted Array/Solution.java
+++ Leet Code/Problems/977. Squares of a Sorted Array/Solution.java
@@ -2,25 +2,11 @@ class Solution
 {
     public int[] sortedSquares(int[] nums)
     {
-        List<Integer> list = new ArrayList<>();
-        int indx=-1,i=0;
-        for(i=0;i<nums.length;i++)
+        int ans[] = new int[nums.length];
+        for(int i=nums.length-1,left=0,right=i;i>=0;i--)
         {
-            if(nums[i]>=0)
-            {
-                list.add(nums[i]*nums[i]);
-            }
-            else
-            {
-                indx=i;
-            }
+            ans[i]=(nums[left]*nums[left]>nums[right]*nums[right])?nums[left]*nums[left++]:nums[right]*nums[right--];
         }
-        i=0;
-        for(;indx>=0;indx--)
-        {
-            for(;i<list.size() && (nums[indx]*nums[indx])>list.get(i);i++);
-            list.add(i,nums[indx]*nums[indx]);
-        }
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return ans;
     }
 }
