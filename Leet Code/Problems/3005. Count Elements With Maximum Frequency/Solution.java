c38514c910e6701c0bc476fee8b509508abab71d
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-03-09 08:01:52 +0530

    Update Solution.java

--- Leet Code/Problems/3005. Count Elements With Maximum Frequency/Solution.java
+++ Leet Code/Problems/3005. Count Elements With Maximum Frequency/Solution.java
@@ -2,21 +2,21 @@ class Solution
 {
     public int maxFrequencyElements(int[] nums)
     {
-        int max=0,cnt=0;
+        int max=0,total=0;
         Map<Integer,Integer> map = new HashMap<>();
         for(int num : nums)
         {
-            int val = map.getOrDefault(num,0)+1;
-            map.put(num,val);
-            max=Integer.max(max,val);
-        }
-        for(Map.Entry<Integer,Integer> ob : map.entrySet())
-        {
-            if(ob.getValue().intValue()==max)
+            int freq = map.getOrDefault(num,0)+1;
+            map.put(num,freq);
+            if(freq>max)
             {
-                cnt++;
+                total=max=freq;
+            }
+            else if(freq==max)
+            {
+                total+=freq;
             }
         }
-        return cnt*max;
+        return total;
     }
 }
