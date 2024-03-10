2c8a75a81968003eedc1eb4cbcbd1a8f2a0e1009
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-03-10 17:19:36 +0530

    Update Solution.java

--- Leet Code/Problems/852. Peak Index in a Mountain Array/Solution.java
+++ Leet Code/Problems/852. Peak Index in a Mountain Array/Solution.java
@@ -2,20 +2,23 @@ class Solution
 {
     public int peakIndexInMountainArray(int[] arr)
     {
-        int left=0;
-        int right=arr.length-1;
-        while(left<right)
+        int mid=0;
+        for(int left=0,right=arr.length-1;left<=right;)
         {
-            int mid=(left+right)/2;
-            if(arr[mid]>arr[mid+1])
+            mid=(left+right)/2;
+            if(arr[mid-1]<arr[mid] && arr[mid]<arr[mid+1])
+            {
+                left=mid;
+            }
+            else if(arr[mid-1]>arr[mid] && arr[mid]>arr[mid+1])
             {
                 right=mid;
             }
             else
             {
-                left=mid+1;
+                break;
             }
         }
-        return left;
+        return mid;
     }
 }
