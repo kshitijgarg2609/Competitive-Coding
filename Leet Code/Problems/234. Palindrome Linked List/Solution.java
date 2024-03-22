aeac8ac585ac8ef7b91df566827d2fef06cba4c1
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-03-22 13:32:46 +0530

    Update Solution.java

--- Leet Code/Problems/234. Palindrome Linked List/Solution.java
+++ Leet Code/Problems/234. Palindrome Linked List/Solution.java
@@ -12,25 +12,23 @@ class Solution
 {
     public boolean isPalindrome(ListNode head)
     {
-       ListNode ptr_slow=head;
-       ListNode ptr_fast=head;
-       ListNode rev_head=null;
-       while(ptr_fast!=null && ptr_fast.next!=null)
-       {
-           rev_head=new ListNode(ptr_slow.val,rev_head);
-           ptr_slow=ptr_slow.next;
-           ptr_fast=ptr_fast.next.next;
-       }
-       ptr_slow=(ptr_fast!=null)?ptr_slow.next:ptr_slow;
-       while(ptr_slow!=null)
-       {
-           if(ptr_slow.val!=rev_head.val)
-           {
-               return false;
-           }
-           ptr_slow=ptr_slow.next;
-           rev_head=rev_head.next;
-       }
-       return true;
+        ListNode fst=head,rev=null;
+        for(;fst!=null && fst.next!=null;fst=fst.next.next)
+        {
+            rev=new ListNode(head.val,rev);
+            head=head.next;
+        }
+        if(fst!=null)
+        {
+            head=head.next;
+        }
+        for(;head!=null;head=head.next,rev=rev.next)
+        {
+            if(head.val!=rev.val)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
