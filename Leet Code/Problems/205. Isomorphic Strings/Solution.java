d934bd62f42a9aae79c41f0ecf1dd8a58a07aeb2
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-04-02 09:55:26 +0530

    Update Solution.java

--- Leet Code/Problems/205. Isomorphic Strings/Solution.java
+++ Leet Code/Problems/205. Isomorphic Strings/Solution.java
@@ -2,17 +2,22 @@ class Solution
 {
     public boolean isIsomorphic(String s, String t)
     {
-        Map<Character,Character> map = new HashMap<>();
+        Map<Character,Character> map1 = new HashMap<>();
+        Map<Character,Character> map2 = new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
-            char c=s.charAt(i);
-            char d=t.charAt(i);
-            boolean key_check=map.containsKey(c);
-            if((key_check && map.get(c)!=d)||(!key_check && map.containsValue(d)))
+            if(!map1.containsKey(s.charAt(i)))
+            {
+                map1.put(s.charAt(i),t.charAt(i));
+            }
+            if(!map2.containsKey(t.charAt(i)))
+            {
+                map2.put(t.charAt(i),s.charAt(i));
+            }
+            if(map1.get(s.charAt(i)).charValue()!=t.charAt(i) || map2.get(t.charAt(i)).charValue()!=s.charAt(i))
             {
                 return false;
             }
-            map.put(c,d);
         }
         return true;
     }
