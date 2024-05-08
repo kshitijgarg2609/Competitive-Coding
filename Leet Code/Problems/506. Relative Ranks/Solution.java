25c1a1a887153433ccb772bb4ee0e20b50147077
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-05-08 10:14:51 +0530

    Update Solution.java

--- Leet Code/Problems/506. Relative Ranks/Solution.java
+++ Leet Code/Problems/506. Relative Ranks/Solution.java
@@ -3,17 +3,13 @@ class Solution
     public String[] findRelativeRanks(int[] score)
     {
         List<String> medals = new ArrayList<>(Arrays.asList("Gold Medal","Silver Medal","Bronze Medal"));
-        SortedSet<Integer> indx = new TreeSet<>(Collections.reverseOrder(Comparator.comparing(x->score[x-1])));
-        for(int i=0;i<score.length;i++)
-        {
-            indx.add(i+1);
-        }
+        SortedSet<Integer> indx = new TreeSet<>(Collections.reverseOrder(Comparator.comparing(x->score[x])));
+        for(int i=0;i<score.length;indx.add(i++));
         String arr[] = new String[score.length];
-        int cnt=1;
+        int rank=0;
         for(int x : indx)
         {
-            arr[x-1]=(medals.size()!=0)?medals.remove(0):Integer.valueOf(cnt).toString();
-            cnt++;
+            arr[x]=(rank++<3)?medals.get(rank-1):String.valueOf(rank);
         }
         return arr;
     }
