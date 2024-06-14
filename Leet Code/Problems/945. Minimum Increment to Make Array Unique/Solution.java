af4213a54cda07c78e6ed55d78f15adcede991a2
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-06-14 10:54:49 +0530

    Create Solution.java

---
+++ Leet Code/Problems/945. Minimum Increment to Make Array Unique/Solution.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int minIncrementForUnique(int[] nums)
+    {
+        Arrays.sort(nums);
+        int moves=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i-1]>=nums[i])
+            {
+                moves+=nums[i-1]+1-nums[i];
+                nums[i]=nums[i-1]+1;
+            }
+        }
+        return moves;
+    }
+}
