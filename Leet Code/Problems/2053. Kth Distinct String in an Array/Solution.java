fab8d7f2493279268813040530fc0d96bf6e8c9c
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-08-05 13:22:14 +0530

    Create Solution.java

---
+++ Leet Code/Problems/2053. Kth Distinct String in an Array/Solution.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    public String kthDistinct(String[] arr, int k)
+    {
+        String distinct = "";
+        Map<String,Integer> map = new HashMap<>();
+        for(String str : arr)
+        {
+            map.put(str,map.getOrDefault(str,0)+1);
+        }
+        for(String str : arr)
+        {
+            if(map.get(str)==1)
+            {
+                if(--k==0)
+                {
+                    distinct=str;
+                    break;
+                }
+            }
+        }
+        return distinct;
+    }
+}
