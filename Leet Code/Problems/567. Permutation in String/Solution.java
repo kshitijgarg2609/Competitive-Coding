81977e549c43083c88bf979a299f9b2e06058d2e
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-10-05 13:33:09 +0530

    Update Solution.java

--- Leet Code/Problems/567. Permutation in String/Solution.java
+++ Leet Code/Problems/567. Permutation in String/Solution.java
@@ -3,23 +3,18 @@ class Solution
     public boolean checkInclusion(String s1, String s2)
     {
         int cnt[] = new int[26];
-        for(char c : s1.toCharArray())
-        {
-            cnt[c-'a']++;
-        }
+        for(int i=0;i<s1.length();cnt[s1.charAt(i++)-'a']++);
         for(int i=0;(i+s1.length())<=s2.length();i++)
         {
-            if(cnt[s2.charAt(i)-'a']!=0)
+            if(cnt[s2.charAt(i)-'a']==0)
             {
-                int arr[] = Arrays.copyOf(cnt,cnt.length);
-                for(int j=i;j<(i+s1.length());j++)
-                {
-                    arr[s2.charAt(j)-'a']--;
-                }
-                if(Arrays.stream(arr).filter(x->(x==0)).count()==cnt.length)
-                {
-                    return true;
-                }
+                continue;
+            }
+            int f[] = Arrays.copyOf(cnt,cnt.length);
+            for(int j=i;j<(i+s1.length());f[s2.charAt(j++)-'a']--);
+            if(Arrays.stream(f).filter(c->c==0).count()==26)
+            {
+                return true;
             }
         }
         return false;
