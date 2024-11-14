efd33c9d53508f4369df08cb6163a443529e492c
Kshitij Garg <kshitijgarg2609@gmail.com>
2024-11-14 16:51:36 +0530

    Create Solution.java

---
+++ Leet Code/Problems/2064. Minimized Maximum of Products Distributed to Any Store/Solution.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public int minimizedMaximum(int n, int[] quantities)
+    {
+        int l=1,r=Arrays.stream(quantities).max().getAsInt(),save=r;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            int cnt=0;
+            for(int q : quantities)
+            {
+                cnt+=(int)(Math.ceil((1.0*q)/mid));
+            }
+            if(cnt>n)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+                save=mid;
+            }
+        }
+        return save;
+    }
+}
