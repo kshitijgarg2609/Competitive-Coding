4bdf845631128dbb24fbbc74379633222878756b
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-01-10 02:38:35 +0530

    Create Solution.java

---
+++ Leet Code/Problems/2185. Counting Words With a Given Prefix/Solution.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int prefixCount(String[] words, String pref)
+    {
+        int cnt=0;
+        for(String w : words)
+        {
+            if(w.startsWith(pref))
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
