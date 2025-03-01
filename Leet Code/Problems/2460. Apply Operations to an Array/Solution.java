bb40b24a78d49576ac94c9c55353ca3f614b4568
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-03-01 17:25:48 +0530

    Create Solution.java

---
+++ Leet Code/Problems/2460. Apply Operations to an Array/Solution.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int[] applyOperations(int[] nums)
+    {
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        return Arrays.copyOf(Arrays.stream(nums).filter(num->num!=0).toArray(),nums.length);
+    }
+}
