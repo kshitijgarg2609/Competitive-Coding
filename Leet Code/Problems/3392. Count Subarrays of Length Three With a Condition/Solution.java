d08fc0698ccab5315d7bb65a4c3dd5284d2cc812
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-04-27 23:08:26 +0530

    Create Solution.java

---
+++ Leet Code/Problems/3392. Count Subarrays of Length Three With a Condition/Solution.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int countSubarrays(int[] nums)
+    {
+        int cnt=0;
+        for(int i=1;i<nums.length-1;i++)
+        {
+            if(nums[i]==2*(nums[i-1]+nums[i+1]))
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
