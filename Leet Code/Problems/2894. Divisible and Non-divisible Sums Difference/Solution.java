16ab210970b617050b3cd1c9574d4263b661f73d
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-05-27 22:19:36 +0530

    Create Solution.java

---
+++ Leet Code/Problems/2894. Divisible and Non-divisible Sums Difference/Solution.java
@@ -0,0 +1,7 @@
+class Solution
+{
+    public int differenceOfSums(int n, int m)
+    {
+        return IntStream.rangeClosed(1,n).filter(i->i%m!=0).sum()-IntStream.rangeClosed(1,n).filter(i->i%m==0).sum();
+    }
+}
