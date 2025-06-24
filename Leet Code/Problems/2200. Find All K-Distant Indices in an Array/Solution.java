04de0cfc1e8d9b4aec399784a0cf5284afde487d
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-06-24 13:30:23 +0530

    Create Solution.java

---
+++ Leet Code/Problems/2200. Find All K-Distant Indices in an Array/Solution.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k)
+    {
+        List<Integer> list = new LinkedList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[j]==key && Math.abs(i-j)<=k)
+                {
+                    list.add(i);
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+}
