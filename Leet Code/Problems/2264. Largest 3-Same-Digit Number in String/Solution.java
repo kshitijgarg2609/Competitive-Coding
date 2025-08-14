e53c703a97025bd7244e9b380f63ae95160a7b2f
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-08-14 13:57:38 +0530

    Update Solution.java

--- Leet Code/Problems/2264. Largest 3-Same-Digit Number in String/Solution.java
+++ Leet Code/Problems/2264. Largest 3-Same-Digit Number in String/Solution.java
@@ -2,21 +2,6 @@ class Solution
 {
     public String largestGoodInteger(String num)
     {
-        boolean vis[] = new boolean[10];
-        for(int i=0;i<num.length();i++)
-        {
-            if(!vis[num.charAt(i)-'0'] && i+1<num.length() && num.charAt(i+1)==num.charAt(i) && i+2<num.length() && num.charAt(i+2)==num.charAt(i))
-            {
-                vis[num.charAt(i)-'0']=true;
-            }
-        }
-        for(int i=vis.length-1;i>=0;i--)
-        {
-            if(vis[i])
-            {
-                return String.format("%d%d%d",i,i,i);
-            }
-        }
-        return "";
+        return IntStream.rangeClosed(0,num.length()-3).mapToObj(i->num.substring(i,i+3)).filter(n->n.charAt(0)==n.charAt(1) && n.charAt(1)==n.charAt(2)).sorted(Collections.reverseOrder()).findFirst().orElse("");
     }
 }
