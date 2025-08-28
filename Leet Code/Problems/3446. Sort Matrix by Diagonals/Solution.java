c7d4bc78ef402cc10817f48d5aeb3b1549f03aea
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-08-28 13:35:38 +0530

    Update Solution.java

--- Leet Code/Problems/3446. Sort Matrix by Diagonals/Solution.java
+++ Leet Code/Problems/3446. Sort Matrix by Diagonals/Solution.java
@@ -7,14 +7,8 @@ class Solution
         for(int start[] : Stream.concat(IntStream.range(0,n).mapToObj(r->new int[]{r,0,-1}),IntStream.range(1,n).mapToObj(c->new int[]{0,c,1})).collect(Collectors.toList()))
         {
             PriorityQueue<Integer> pq = (start[2]==-1)?(new PriorityQueue<>(Collections.reverseOrder())):(new PriorityQueue<>());
-            for(int i=start[0],j=start[1];i<n && j<n;i++,j++)
-            {
-                pq.add(grid[i][j]);
-            }
-            for(int i=start[0],j=start[1];i<n && j<n;i++,j++)
-            {
-                arr[i][j]=pq.remove();
-            }
+            for(int i=start[0],j=start[1];i<n && j<n;pq.add(grid[i++][j++]));
+            for(int i=start[0],j=start[1];i<n && j<n;arr[i++][j++]=pq.remove());
         }
         return arr;
     }
