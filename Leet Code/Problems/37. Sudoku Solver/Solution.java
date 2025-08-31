019d3c49bbb5d25a48cf540dcc2f687cdf8e1c4d
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-08-31 22:46:08 +0530

    Update Solution.java

--- Leet Code/Problems/37. Sudoku Solver/Solution.java
+++ Leet Code/Problems/37. Sudoku Solver/Solution.java
@@ -2,44 +2,51 @@ class Solution
 {
     public void solveSudoku(char[][] board)
     {
-        List<int[]> blank = new ArrayList<>();
-        boolean row[][] = new boolean[9][9];
-        boolean col[][] = new boolean[9][9];
-        boolean block[][] = new boolean[9][9];
+        boolean row[][] = new boolean[9][10];
+        boolean col[][] = new boolean[9][10];
+        boolean box[][][] = new boolean[3][3][10];
+        Deque<int[]> dq = new LinkedList<>();
+        Stack<int[]> stack = new Stack<>();
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
                 if(board[i][j]=='.')
                 {
-                    blank.add(new int[]{i,j});
-                    continue;
+                    dq.addLast(new int[]{i,j});
                 }
-                row[i][board[i][j]-'0'-1]=col[j][board[i][j]-'0'-1]=block[3*(i/3)+(j/3)][board[i][j]-'0'-1]=true;
+                else
+                {
+                    row[i][board[i][j]-'0']=col[j][board[i][j]-'0']=box[i/3][j/3][board[i][j]-'0']=true;
+                }
             }
         }
-        for(int k=0;k<blank.size();)
+        while(!dq.isEmpty())
         {
-            int start=1,i=blank.get(k)[0],j=blank.get(k)[1];
-            if(board[i][j]!='.')
+            int i=dq.peekFirst()[0],j=dq.peekFirst()[1];
+            char c=board[i][j];
+            if(c=='.')
             {
-                start=board[i][j]-'0'+1;
-                row[i][board[i][j]-'0'-1]=col[j][board[i][j]-'0'-1]=block[3*(i/3)+(j/3)][board[i][j]-'0'-1]=false;
-                board[i][j]='.';
+                c='1';
             }
-            for(;start<=9;start++)
+            else
             {
-                if(!row[i][start-1] && !col[j][start-1] && !block[3*(i/3)+(j/3)][start-1])
+                row[i][c-'0']=col[j][c-'0']=box[i/3][j/3][c-'0']=false;
+                c=(char)(c+1);
+            }
+            for(board[i][j]='.';c<='9';c=(char)(c+1))
+            {
+                if(!row[i][c-'0'] && !col[j][c-'0'] && !box[i/3][j/3][c-'0'])
                 {
-                    row[i][start-1]=col[j][start-1]=block[3*(i/3)+(j/3)][start-1]=true;
-                    board[i][j]=(char)(start+'0');
-                    k++;
+                    board[i][j]=c;
+                    row[i][c-'0']=col[j][c-'0']=box[i/3][j/3][c-'0']=true;
+                    stack.push(dq.remove());
                     break;
                 }
             }
             if(board[i][j]=='.')
             {
-                k--;
+                dq.addFirst(stack.pop());
             }
         }
     }
