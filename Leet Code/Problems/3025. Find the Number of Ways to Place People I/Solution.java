5cde7dad0f86e0e0df834bb30a8f7fbe295921e1
Kshitij Garg <kshitijgarg2609@gmail.com>
2025-09-02 16:23:22 +0530

    Create Solution.java

---
+++ Leet Code/Problems/3025. Find the Number of Ways to Place People I/Solution.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public int numberOfPairs(int[][] points)
+    {
+        int cnt=0;
+        for(int ul[] : points)
+        {
+            for(int lr[] : points)
+            {
+                if(!Arrays.equals(ul,lr) && ul[0]<=lr[0] && ul[1]>=lr[1])
+                {
+                    if(Arrays.stream(points).filter(p->!Arrays.equals(p,ul) && !Arrays.equals(p,lr)).filter(p->ul[0]<=p[0] && p[0]<=lr[0] && ul[1]>=p[1] && p[1]>=lr[1]).count()==0L)
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
