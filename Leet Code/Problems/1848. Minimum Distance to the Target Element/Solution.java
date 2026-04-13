e628d6f7fdb8ae5168036c31ae02d50fd3dfc41d
Kshitij Garg <kshitijgarg2609@gmail.com>
2026-04-13 15:29:50 +0530

    Create Solution.java

---
+++ Leet Code/Problems/1848. Minimum Distance to the Target Element/Solution.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int getMinDistance(int[] nums, int target, int start)
+    {
+        int min=nums.length;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                min=Integer.min(min,Math.abs(i-start));
+            }
+        }
+        return min;
+    }
+}
