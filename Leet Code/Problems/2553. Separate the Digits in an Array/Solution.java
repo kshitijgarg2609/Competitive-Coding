89a784ca5d5835612d0b0832d613ce1c5efe196c
Kshitij Garg <kshitijgarg2609@gmail.com>
2026-05-12 00:20:03 +0530

    Create Solution.java

---
+++ Leet Code/Problems/2553. Separate the Digits in an Array/Solution.java
@@ -0,0 +1,7 @@
+class Solution
+{
+    public int[] separateDigits(int[] nums)
+    {
+        return Arrays.stream(nums).flatMap(num->String.valueOf(num).chars().map(c->c-'0')).toArray();
+    }
+}
