cffe2e6e870aa1f05529aedfc3259a91264d2b3b
Kshitij Garg <kshitijgarg2609@gmail.com>
2026-06-06 13:44:41 +0530

    Update Solution.java

--- Leet Code/Problems/2574. Left and Right Sum Differences/Solution.java
+++ Leet Code/Problems/2574. Left and Right Sum Differences/Solution.java
@@ -3,12 +3,6 @@ class Solution
     public int[] leftRightDifference(int[] nums)
     {
         int lr[] = new int[]{0,Arrays.stream(nums).sum()};
-        return Arrays.stream(nums).map(num->
-        {
-            lr[1]-=num;
-            int sum=Math.abs(lr[0]-lr[1]);
-            lr[0]+=num;
-            return sum;
-        }).toArray();
+        return Arrays.stream(nums).map(num->Math.abs((lr[0]+=num)-num-(lr[1]-=num))).toArray();
     }
 }
