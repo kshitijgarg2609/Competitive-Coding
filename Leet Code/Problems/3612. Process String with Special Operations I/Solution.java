97ca1cec15249faebf4e5563d0f0d404fe367ecc
Kshitij Garg <kshitijgarg2609@gmail.com>
2026-06-16 22:40:25 +0530

    Create Solution.java

---
+++ Leet Code/Problems/3612. Process String with Special Operations I/Solution.java
@@ -0,0 +1,33 @@
+class Solution
+{
+    public String processStr(String s)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray())
+        {
+            if(c=='*')
+            {
+                if(sb.length()>0)
+                {
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+            else if(c=='#')
+            {
+                for(char d : sb.toString().toCharArray())
+                {
+                    sb.append(d);
+                }
+            }
+            else if(c=='%')
+            {
+                sb.reverse();
+            }
+            else
+            {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
