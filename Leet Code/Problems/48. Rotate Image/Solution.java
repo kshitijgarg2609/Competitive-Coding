8281e324ef71f6e07291b4156c32ac882144f2a6
Kshitij Garg <kshitijgarg2609@gmail.com>
2026-07-02 01:12:14 +0530

    Create Solution.java

---
+++ Leet Code/Problems/48. Rotate Image/Solution.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    public void rotate(int[][] matrix)
+    {
+        int n=matrix.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                int tmp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=tmp;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0,k=n-1;j<k;j++,k--)
+            {
+                int tmp=matrix[i][j];
+                matrix[i][j]=matrix[i][k];
+                matrix[i][k]=tmp;
+            }
+        }
+    }
+}
