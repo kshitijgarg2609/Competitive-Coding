d77203317fe2a5bf28c81e71d9dd369bae8d3fa8
Kshitij Garg <kshitijgarg2609@gmail.com>
2026-07-31 14:49:06 +0530

    Update Solution.java

--- Leet Code/Problems/3016. Minimum Number of Pushes to Type Word II/Solution.java
+++ Leet Code/Problems/3016. Minimum Number of Pushes to Type Word II/Solution.java
@@ -2,29 +2,9 @@ class Solution
 {
     public int minimumPushes(String word)
     {
-        int cnt=0,key=2,press=1;
-        Map<Character,Integer> map = new HashMap<>();
-        for(char c : word.toCharArray())
-        {
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-        Map<Integer,Integer> freq = new TreeMap<>(Collections.reverseOrder());
-        for(int f : map.values())
-        {
-            freq.put(f,freq.getOrDefault(f,0)+1);
-        }
-        for(Map.Entry<Integer,Integer> ent : freq.entrySet())
-        {
-            for(int i=0;i<ent.getValue();i++)
-            {
-                cnt+=(ent.getKey()*press);
-                if(key++==9)
-                {
-                    key=2;
-                    press++;
-                }
-            }
-        }
-        return cnt;
+        int cnt[] = new int[26];
+        word.chars().forEach(c->cnt[c-'a']++);
+        Arrays.sort(cnt);
+        return IntStream.range(0,26).map(i->((i/8)+1)*cnt[26-i-1]).sum();
     }
 }
